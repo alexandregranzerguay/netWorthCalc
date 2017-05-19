e7f203907189a9e5fd1dfec583e380d85b531b9f
alexandregranzerguay <alexandre.granzerguay@gmail.com>
2017-05-19 11:18:32 -0400

    Fix basic mathimatical calculations

--- netWorthCalc/public/js/controllers/controller.js
+++ netWorthCalc/public/js/controllers/controller.js
@@ -4,10 +4,10 @@
 var cont = angular.module('ic', []);
 cont.controller('Ctrl', function informationController($scope){
  	$scope.calcData = function(){  
- 	var debt = $scope.debt;
-    var age = $scope.age;
-    var yrsgrad = $scope.yrsgrad;
-    var yrspay = $scope.yrspay;
+ 	var debt = Number($scope.debt);
+    var age = Number($scope.age);
+    var yrsgrad = Number($scope.yrsgrad);
+    var yrspay = Number($scope.yrspay);
 
     if(debt > 0 && age > 0 && yrsgrad > 0 && yrspay > 0){
         var price = debt + age + yrsgrad + yrspay;
